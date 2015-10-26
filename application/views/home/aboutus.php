8c6dff001bd0bd92caab468430511e4447cf86b0
MonkeyKing <MonkeyKing1024@gmail.com>
2015-10-26 17:22:22 +0800

    update

--- application/views/home/aboutus.php
+++ application/views/home/aboutus.php
@@ -5,10 +5,10 @@
 	<meta name="keywords" content="HeyJuice,轻断食果汁,纤体,冷榨蔬果汁,轻断食蔬果汁,HeyJuice轻断食蔬果汁,果汁,果蔬汁,轻断食果蔬汁,冷榨果汁">
 	<meta name="description" content="轻断食果蔬汁HeyJuice，3天变美计划。营养学家设计的冷压鲜榨果蔬汁餐单，每天6瓶连续3天，排出体内毒素，激发细胞修复力，减去多余脂肪、美颜、身体变轻盈，充满青春活力！新鲜水果与蔬菜冷压鲜榨，全程冷链免费送达。">
 	<title>轻断食果蔬汁沛时，3天变美计划</title>
-	<script src="index_files/v.htm" charset="utf-8"></script><script src="js/ntkfstat.js" charset="utf-8" async="" type="text/javascript"></script><script type="text/javascript" src="js/base.js"></script>
-	<script type="text/javascript" src="js/common.js"></script>
-	<script type="text/javascript" src="js/global.js"></script>
-	<script type="text/javascript" src="js/jquery.js"></script>
+	<script src="/assets/index_files/v.htm" charset="utf-8"></script><script src="/assets/js/ntkfstat.js" charset="utf-8" async="" type="text/javascript"></script><script type="text/javascript" src="/assets/js/base.js"></script>
+	<script type="text/javascript" src="/assets/js/common.js"></script>
+	<script type="text/javascript" src="/assets/js/global.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery.js"></script>
 	<script type="text/javascript">
 	var NTKF_PARAM = {
 		siteid:"kf_9700",                   
@@ -21,10 +21,10 @@
 	  	userlevel:""
 	} 
 	</script>
-	<script src="" type="text/javascript" charset="utf-8"></script>
-	<link rel="stylesheet" type="text/css" href="css/global.css">
-	<link rel="stylesheet" type="text/css" href="css/base.css">
-	<script src="index_files/userinfo.php" charset="utf-8" async="async" type="text/javascript"></script><script data-requiremodule="lang" src="js/zh_cn.js" charset="utf-8" async="async" type="text/javascript"></script><script data-requiremodule="chatManage" src="js/chat.js" charset="utf-8" async="async" type="text/javascript"></script><script data-requiremodule="Window" src="js/nt2.js" charset="utf-8" async="async" type="text/javascript"></script></head>
+	<script src="/assets/" type="text/javascript" charset="utf-8"></script>
+	<link rel="stylesheet" type="text/css" href="/assets/css/global.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/base.css">
+	<script src="index_files/userinfo.php" charset="utf-8" async="async" type="text/javascript"></script><script data-requiremodule="lang" src="/assets/js/zh_cn.js" charset="utf-8" async="async" type="text/javascript"></script><script data-requiremodule="chatManage" src="/assets/js/chat.js" charset="utf-8" async="async" type="text/javascript"></script><script data-requiremodule="Window" src="/assets/js/nt2.js" charset="utf-8" async="async" type="text/javascript"></script></head>
 	<body>
 		<div class="wrap Wrap">
 			<div class="navtop">
@@ -39,15 +39,15 @@
 				<div class="heard_warp clearfix">
 					<div class="logo">
 						<a href="/"></a>
-						<img src="images/re_index_logo.png" width="70px">
+						<img src="/assets/images/re_index_logo.png" width="70px">
 					</div>
 					<div class="header_bar HeaderBar">
 						<ul>
-							<li><a class="this" href="index.htm">首页</a></li>
-							<li><a class="" href="product.htm">产品</a></li>
-							<li><a class="" href="comment.htm">论坛</a></li>
-							<li><a class="" href="brand.htm">品牌活动</a></li>
-							<li><a class="" href="service.htm">品牌理念</a></li>
+							<li><a class="this" href="/">首页</a></li>
+							<li><a class="" href="/home/productlist">产品</a></li>
+							<li><a class="" href="/home/comment">论坛</a></li>
+							<li><a class="" href="/home/brand">品牌活动</a></li>
+							<li><a class="" href="/home/service">品牌理念</a></li>
 						</ul>
 						<div class="shoping_car">
 							<a class="ShoppingCarLink" href="">
@@ -58,8 +58,8 @@
 				</div>
 			</div>
 							<div class="about_nav">
-				<a href="aboutus.htm" class="this">关于我们</a>
-				<a href="contactus.htm" class="">联系我们</a>
+				<a href="/home/aboutus" class="this">关于我们</a>
+				<a href="/home/contactus" class="">联系我们</a>
 			</div>
 							<div class="main">
 				
@@ -72,7 +72,7 @@
 				<div class="about_tree_content">
 					复合果汁上市
 					<div class="about_tree_icon"></div>
-					<img src="images/about_tree_box10.jpg" alt="">
+					<img src="/assets/images/about_tree_box10.jpg" alt="">
 				</div>
 			</div>
 			<div class="about_tree_box box21 left bg_b">
@@ -81,7 +81,7 @@
 				<div class="about_tree_content">
 					沛时线下销售门店到达50家
 					<div class="about_tree_icon"></div>
-					<img src="images/about_tree_box9.jpg" alt="" width="150px">
+					<img src="/assets/images/about_tree_box9.jpg" alt="" width="150px">
 				</div>
 			</div>
 			<div class="about_tree_year box19"><span>2014</span></div>
@@ -99,7 +99,7 @@
 				<div class="about_tree_content">
 					沛时生果汁首发
 					<div class="about_tree_icon"></div>
-					<img src="images/about_tree_box18.jpg" alt="" width="150px;">
+					<img src="/assets/images/about_tree_box18.jpg" alt="" width="150px;">
 				</div>
 			</div>
 
@@ -130,7 +130,7 @@
 					"沛时"亮相北京国际电影节
 					<div class="about_tree_icon"></div>
 				</div>
-				<img src="images/about_tree_box6.jpg" alt="">
+				<img src="/assets/images/about_tree_box6.jpg" alt="">
 			</div>
 		
 		
@@ -152,7 +152,7 @@
 					第一台冷压榨取到京
 					<div class="about_tree_icon"></div>
 				</div>
-				<img src="images/delivery_page_14.png" alt="">
+				<img src="/assets/images/delivery_page_14.png" alt="">
 			</div>
 		</div>
 	</div>
@@ -162,7 +162,7 @@
 			</div>
 				<div class="footer">
 							<div class="footer_logo" >
-								<img src="images/footer_logo.jpg" type="image/svg+xml" width="280px" >
+								<img src="/assets/images/footer_logo.jpg" type="image/svg+xml" width="280px" >
 							</div>
 							<div class="footer_info">
 								<div class="eq_code">
@@ -175,9 +175,9 @@
 									<p class="about_h1"style="margin-top:10px;">联系“沛时”获得在线支持</p>
 									<p class="f12">7x13小时专心服务,请通过任意方式联系我们</p>
 									<ul class="iconfont" style="margin-top:20px;">
-										<li><img src="images/weixin.png"></li>
-										<li><img src="images/weibo.png"></li>
-										<li><img src="images/taobao.png"></li>
+										<li><img src="/assets/images/weixin.png"></li>
+										<li><img src="/assets/images/weibo.png"></li>
+										<li><img src="/assets/images/taobao.png"></li>
 									</ul>	
 								</div>
 								<div class="better_me" style="margin-top:-26px;">
@@ -187,10 +187,10 @@
 						</div>
 						<div class="friendly_link">
 							<ul>
-								<li><a href="aboutus.htm">关于沛时</a></li>
-								<li><a href="contactus.htm">联系我们</a></li>
-								<li><a href="help.htm">帮助中心</a></li>
-								<li><a href="brand.htm">品牌活动</a></li>
+								<li><a href="/home/aboutus">关于沛时</a></li>
+								<li><a href="/home/contactus">联系我们</a></li>
+								<li><a href="/home/help">帮助中心</a></li>
+								<li><a href="/home/brand">品牌活动</a></li>
 								<li><a href="http://www.heyjuice.cn/Delivery/index"></a></li>
 							</ul>
 						</div>
@@ -201,7 +201,7 @@
 		<script type="text/javascript">
 		var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://":" http://");
 		document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fe1e9293140df80038214b7e8ebdc4ab7' type='text/javascript'%3E%3C/script%3E"));
-		</script><script src="js/h.js" type="text/javascript"></script><a href="http://tongji.baidu.com/hm-web/welcome/ico?s=e1e9293140df80038214b7e8ebdc4ab7" target="_blank"><img src="images/21.gif" border="0" height="20" width="20"></a>
+		</script><script src="/assets/js/h.js" type="text/javascript"></script><a href="http://tongji.baidu.com/hm-web/welcome/ico?s=e1e9293140df80038214b7e8ebdc4ab7" target="_blank"><img src="/assets/images/21.gif" border="0" height="20" width="20"></a>
 		</div>
 		<script type="text/javascript" charset="utf-8">
 		  $(function() {
