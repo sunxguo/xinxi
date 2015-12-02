24b04c1a02da6db1005f496e9cfc960fd093d21f
MonkeyKing <MonkeyKing1024@gmail.com>
2015-12-02 10:27:05 +0800

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,44 +8,45 @@ class Home extends CI_Controller {
 		$this->load->library('GetData');
 	}
 	public function index(){
-		$data=array();
-		//滚动
-		$parameters=array(
-			'result'=>'data',
-			'column'=>1,
-			'orderBy'=>array('time'=>'DESC')
-		);
-		$data['sliders']=$this->getdata->getEssays($parameters);
-		//轻断食果蔬汁
-		$parameters=array(
-			'result'=>'data',
-			'column'=>2,
-			'orderBy'=>array('time'=>'DESC')
-		);
-		$data['qingduanguoshus']=$this->getdata->getEssays($parameters);
-		//沛时左侧滚动图
-		$parameters=array(
-			'result'=>'data',
-			'column'=>3,
-			'orderBy'=>array('time'=>'DESC')
-		);
-		$data['peishizuoce']=$this->getdata->getEssays($parameters);
-		//沛时右侧'论坛'最新三条
-		$parameters=array(
-			'result'=>'data',
-			'column'=>5,
-			'limit'=>array('limit'=>3,'offset'=>0),
-			'orderBy'=>array('time'=>'DESC')
-		);
-		$data['comment']=$this->getdata->getEssays($parameters);
-		//'品牌活动'最新三条
-		$parameters=array(
-			'result'=>'data',
-			'column'=>6,
-			'limit'=>array('limit'=>3,'offset'=>0),
-			'orderBy'=>array('time'=>'DESC')
-		);
-		$data['pinpaihuodong']=$this->getdata->getEssays($parameters);
+		$data=array('about'=>'');
+
+		// //滚动
+		// $parameters=array(
+		// 	'result'=>'data',
+		// 	'column'=>1,
+		// 	'orderBy'=>array('time'=>'DESC')
+		// );
+		// $data['sliders']=$this->getdata->getEssays($parameters);
+		// //轻断食果蔬汁
+		// $parameters=array(
+		// 	'result'=>'data',
+		// 	'column'=>2,
+		// 	'orderBy'=>array('time'=>'DESC')
+		// );
+		// $data['qingduanguoshus']=$this->getdata->getEssays($parameters);
+		// //沛时左侧滚动图
+		// $parameters=array(
+		// 	'result'=>'data',
+		// 	'column'=>3,
+		// 	'orderBy'=>array('time'=>'DESC')
+		// );
+		// $data['peishizuoce']=$this->getdata->getEssays($parameters);
+		// //沛时右侧'论坛'最新三条
+		// $parameters=array(
+		// 	'result'=>'data',
+		// 	'column'=>5,
+		// 	'limit'=>array('limit'=>3,'offset'=>0),
+		// 	'orderBy'=>array('time'=>'DESC')
+		// );
+		// $data['comment']=$this->getdata->getEssays($parameters);
+		// //'品牌活动'最新三条
+		// $parameters=array(
+		// 	'result'=>'data',
+		// 	'column'=>6,
+		// 	'limit'=>array('limit'=>3,'offset'=>0),
+		// 	'orderBy'=>array('time'=>'DESC')
+		// );
+		// $data['pinpaihuodong']=$this->getdata->getEssays($parameters);
 		$this->load->view('home/index',$data);
 	}
 	public function product(){
---
+++ application/views/home/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- <meta http-equiv="Content-Type" content="text/html;charset=gb2312" /> -->
+	<title>403 Forbidden</title>
+</head>
+<body>
+
+<?php //print_r(iconv("UTF-8", "GB2312//IGNORE", $about[0]->topic)); 
+//echo $about->contents;?>
+
+</body>
+</html>
